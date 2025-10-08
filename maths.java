3c63e01b6496b21a5a2590611fb3972248541171
bhanuprakash0907 <bhanprakash0907@gmail.com>
2025-10-08 10:19:44 +0530

    Add files via upload

---
+++ maths.java
@@ -0,0 +1,20 @@
+class maths
+{
+ public static void main(String args[])
+ {
+ System.out.println("🥰️😃️");
+  System.out.println("Maximum value is " +Math.max(6,9));
+  System.out.println("Minimum value is " +Math.min(6,9));
+  System.out.println("Absolute value is " +Math.abs(-49));
+  System.out.println("Approximate value is " +Math.round(5.5));
+  System.out.println("ceil value is " +Math.ceil(26.5));
+  System.out.println("floor value is " +Math.floor(26.5));
+  System.out.println("square root value is " +Math.sqrt(729));
+  System.out.println("Cube root value is " +Math.cbrt(64));
+  System.out.println("Maximum value is " +Math.max(6,9));
+  System.out.println("Random value is " +(int)(Math.random()*100));
+  System.out.println("Random value is " +Math.random()*100);
+ }
+ }
+ 
+
